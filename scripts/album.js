35469112e0fd7cd9bf9ad33a68f656363b747407
amhayslip <amhayslip@gmail.com>
2017-12-20 10:19:43 -0500

    initial commit

---
+++ scripts/album.js
@@ -0,0 +1,35 @@
+var createSongRow = function (songNumber, songName, songLength) {
+  var template =
+     '<tr class="album-view-song-item">'
+   + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
+   + '  <td class="song-item-title">' + songName + '</td>'
+   + '  <td class="song-item-duration">' + songLength + '</td>'
+   + '</tr>'
+   ;
+
+   var $row = $(template);
+
+   return $row;
+};
+
+var setCurrentAlbum = function(album) {
+  currentAlbum = album;
+
+  var $albumTitle = $('.album-view-title');
+  var $albumArtist = $('.album-view-artist');
+  var $albumReleaseInfo = $('.album-view-release-info');
+  var $albumImage = $('.album-cover-art');
+  var $albumSongList = $('.album-view-song-list');
+
+  $albumTitle.text(album.title);
+  $albumArtist.text(album.artist);
+  $albumReleaseInfo.text(album.year + ' ' + album.label);
+  $albumImage.attr('src', album.albumArtUrl);
+
+  $albumSongList.empty();
+
+  for (var i = 0; i < album.songs.length; i++) {
+    var $songRow = createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+    $albumSongList.append($songRow);
+  }
+};
---
+++ scripts/data.js
@@ -0,0 +1,37 @@
+var albums = [
+  {
+    title: 'Struggle Pretty',
+    artist: 'Penny and Sparrow',
+    label: 'Marmoset',
+    year: '2014',
+    albumArtUrl: 'assets/images/album_covers/02.jpg',
+    songs: [
+      { title: 'Jeffrey Alan', duration: 60, audioUrl: 'assets/music/jeffrey-alan' },
+      { title: 'Serial Doubter', duration: 204, audioUrl: 'assets/music/serial-doubter' },
+      { title: 'Bread and Bleeding', duration: 256, audioUrl: 'assets/music/bread-and-bleeding' },
+      { title: 'Rattle', duration: 269, audioUrl: 'assets/music/rattle' },
+      { title: 'To Haunt, to Startle', duration: 294, audioUrl: 'assets/music/to-haunt-to-startle' },
+      { title: 'The Literal Heart', duration: 360, audioUrl: 'assets/music/the-literal-heart' },
+      { title: 'Stoic', duration: 144, audioUrl: 'assets/music/stoic' },
+      { title: 'Undress', duration: 307, audioUrl: 'assets/music/undress' },
+      { title: 'Reether', duration: 257, audioUrl: 'assets/music/reether' },
+      { title: 'Thunder', duration: 182, audioUrl: 'assets/music/thunder' },
+      { title: 'Honest Wage', duration: 388, audioUrl: 'assets/music/honest-wage' },
+      { title: 'Fantine', duration: 102, audioUrl: 'assets/music/fantine' }
+    ]
+  },
+  {
+    title: 'Epic Songs',
+    artist: 'Various',
+    label: 'Virgin',
+    year: '2017',
+    albumArtUrl: 'assets/images/album_covers/01.jpg',
+    songs: [
+      { title: 'Alex Che', duration: 93, audioUrl: 'assets/music/alex' },
+      { title: 'Aliaksei Yukhnevich', duration: 111, audioUrl: 'assets/music/aliaksei' },
+      { title: 'Carols Estella', duration: 130, audioUrl: 'assets/music/carlos' },
+      { title: 'Soundbay', duration: 142, audioUrl: 'assets/music/soundbay' },
+      { title: 'Titan', duration: 32, audioUrl: 'assets/music/titan' }
+    ]
+  }
+]
